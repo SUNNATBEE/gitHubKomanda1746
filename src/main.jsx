b6bf5fb6cddd180be113a1346003b2b0e907518f
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2025-12-02 11:08:11 +0500

    Router qoshildi

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import Signup from "./pages/registers/Signup.jsx"
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    children:[
+      {
+        path:"/contact",
+        element:<Contact/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/signup",
+        element:<Signup/>
+      }
+    ]
   },
 ]);
 
